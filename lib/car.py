a57b366a4095b7dc41d749c1dbff1cf24403d123
Donell <donell.wambua@student.moringaschool.com>
2023-08-26 23:51:31 +0300

    Completed assignment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,14 @@
 from vehicle import Vehicle
+from car import Car
 
-class Car(Vehicle):
-    pass
+class TestVehicleAndCar:
+    def test_vehicle_attributes(self):
+        vehicle = Vehicle(wheel_size=18, wheel_number=4)
+        assert vehicle.wheel_size == 18
+        assert vehicle.wheel_number == 4
+
+    def test_vehicle_go_method(self):
+        vehicle = Vehicle(wheel_size=18, wheel_number=4)
+        assert vehicle.go() == "vrrrrrrrooom!"
+
+    # ...similar tests for Car class...
